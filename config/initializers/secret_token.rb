8838d37a9d141b06ff7effe664ee1de445972f04
John Kearney <john.kearney@swrve.com>
2011-11-12 13:54:02 -0800

    initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WwwJohnmarkkearneyCom::Application.config.secret_token = '9ed77d3ddb547dc9dd008604463c6c77d50be59eb37b2e0972c9c4fbf73f2e42b5e429f3413b6a06636a18e56409566120940d00fab5c0f6a41e39c95ce4b087'
